df64660e274e6b5722adce55d2c52e289ada3cff
Mohamad <taufiqrhmd@gmail.com>
2025-03-14 19:27:06 +0700

    popup(modals) page billing, dan shift kerja

--- resources/views/Kasir/layout-kasir.blade.php
+++ resources/views/Kasir/layout-kasir.blade.php
@@ -10,7 +10,7 @@
 
 <body style="background-color: #F5F5F5">
     <div class="flex flex-col md:flex-row h-screen">
-        @include('component.sidebar-kasir')
+        @include('components.sidebar-kasir')
         <div class="w-full md:w-full pt-6 mx-auto overflow-auto" id="content-area">
             <div class="w-full bg-gray-100 py-3 mb-4 flex flex-wrap justify-between items-center px-4 md:px-6">
                 <div class="text-lg md:text-2xl text-gray-500">
@@ -19,7 +19,7 @@
                 <div class="flex items-center text-black mt-2 md:mt-0">
                     {{ Auth::user()->name ?? 'Guest' }}
                     <span class="ml-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                        <svg onclick="openModal('modalMulaiShift')" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                             fill="none" stroke="#818181" stroke-width="2" stroke-linecap="round"
                             stroke-linejoin="round">
                             <path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
@@ -34,6 +34,8 @@
             </div>
         </div>
     </div>
+
+    @include('components.popup-kasir')
 </body>
 
 </html>
